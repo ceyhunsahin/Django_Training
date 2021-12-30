3eeb6c320d26b956eb6a4046626f5f24be186971
ceyhunsahin <csahin17072010@gmail.com>
2021-12-30 16:50:21 +0100

    todo_app_1

--- TODO_APP/TODO_APP/settings.py
+++ TODO_APP/TODO_APP/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4=t*1lkqw$ve-16pp_oo6&oskd4gfnpill81(*xpqq1qu%+7h5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
