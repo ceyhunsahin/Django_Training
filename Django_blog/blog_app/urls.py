d2ff3c4a24243a861e0c4e1f48ca8b04753c0680
ceyhunsahin <csahin17072010@gmail.com>
2022-01-07 09:47:54 +0100

    blog_5

--- Django_blog/blog_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import home_view
-
-urlpatterns = [
-    path('home/', home_view)
-]
--- Django_blog/blog_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def home_view(request):
-    return render(request, 'pages/home_page.html')
-
-
-# Create your views here.
