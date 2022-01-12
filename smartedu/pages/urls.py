cbbbbf8660e7311f235c5dfc5bfa2e422661cb15
ceyhunsahin <csahin17072010@gmail.com>
2022-01-12 19:49:34 +0100

    Youtube_example

---
+++ smartedu/pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('index/', views.index, name = 'index'),
+    path('about/', views.about, name = 'about'),
+    path('courses/', views.courses, name = 'courses'),
+
+
+]
